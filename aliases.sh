0a1c056651153201bc00befd3a368d002d05fb63
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2022-08-20 20:57:10 -0300

    Add generic composer command through docker

--- aliases.sh
+++ aliases.sh
@@ -3,6 +3,15 @@
 alias sail="./vendor/bin/sail"
 
 ## Composer
+function composer(){
+  docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/var/www/html \
+    -w /var/www/html \
+    laravelsail/php81-composer:latest \
+    composer $@
+}
+
 alias composer:install="docker run --rm \
     -u "$(id -u):$(id -g)" \
     -v $(pwd):/var/www/html \
